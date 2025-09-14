72c857e386b2f1e5fe5c4e655426a440bfcd0916
dkphhh <dkphhh@foxmail.com>
2025-09-14 15:45:42 +0800

    update

--- README.md
+++ README.md
@@ -10,14 +10,11 @@ Tech stack：Astro + Svelte + TailwindCSS + DaisyUI
 
 ## TODO
 
-TODO：继续研究 pagefind 的用法，尤其是搜索结果的展示
+TODO: 写一个 404 页面
+TODO: 黑夜模式主题
 
 ### TODO - 2.0
 
 TODO: 给所有的 <a></a> 加一个 hover 显示预览效果
 
 TODO: 自定义一个音频展示的样式，暂时的想法是做成一个圆角矩形的按钮，重点是原型的暂停/考试按钮，圆周用红色展示播放进度
-
-
-
-
--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,36 @@ export const onRequest = defineMiddleware(async (context, next) => {
   const { pathname } = context.url;
   const { params, redirect } = context;
 
-  // 检查是否是标签页路由，并且 tag 参数与它的小写版本不符
+  // --- 检查路径是否为 /category1/category2/slug 模式---
+  const categories = ["reading", "writing", "lifelog"];
+  // 将路径分割成数组，并过滤掉空字符串，例如 "/writing/reading/slug" -> ["writing", "reading", "slug"]
+  const pathSegments = pathname.split("/").filter(Boolean);
+
+  // 检查路径是否至少包含两个部分，且前两个部分都是分类目录
+  if (
+    pathSegments.length >= 2 &&
+    categories.includes(pathSegments[0]) &&
+    categories.includes(pathSegments[1])
+  ) {
+    // 这是一个错误的嵌套分类路径，例如 /writing/reading/slug
+    // 我们需要重定向到正确的路径，例如 /reading/slug
+    const newPath = `/${pathSegments.slice(1).join("/")}`;
+    return redirect(newPath, 301); // 301 永久重定向
+  }
+
+  // --- 检查路径是否为 /category/slug1/slug2 格式 ---
+  if (
+    pathSegments.length === 3 &&
+    categories.includes(pathSegments[0]) &&
+    !categories.includes(pathSegments[1]) // 确保中间部分不是一个分类
+  ) {
+    // 这是一个错误的同分类相对路径，例如 /writing/slug-a/slug-b
+    // 我们需要重定向到正确的路径，例如 /writing/slug-b
+    const newPath = `/${pathSegments[0]}/${pathSegments[2]}`;
+    return redirect(newPath, 301); // 301 永久重定向
+  }
+
+  // --- 检查是否是标签页路由，并且 tag 参数与它的小写版本不符---
   if (
     pathname.startsWith("/tags/") &&
     params.tag &&
@@ -21,6 +50,7 @@ export const onRequest = defineMiddleware(async (context, next) => {
     return redirect(lowercasePath, 301); // 301 表示永久重定向
   }
 
+  // --- about页面重定向 ---
   if (pathname === "/about") {
     return redirect("/lifelog/about-me", 301);
   }
